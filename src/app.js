537c335aa15c1c74b8fac3a875179b9798772383
suhuq007@gmail.com <suhuq007@gmail.com>
2023-03-30 20:44:28 +0530

    moved registration logic to app.js

--- src/app.js
+++ src/app.js
@@ -28,6 +28,17 @@ function generateMarkup() {
 }
 
 function init() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../sw.js')
+      .then(registration => {
+        console.debug('Service worker registered', registration);
+      })
+      .catch(err => console.error('Service worker registration failed ', err))
+  } else {
+    console.error('Service worker not supported');
+  }
+
+
   generateMarkup();
 }
 
